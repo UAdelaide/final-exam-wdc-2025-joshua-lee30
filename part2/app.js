4fa756d2c0bc1d9d8a617b97437879f1e912603e
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-20 05:51:14 +0000

    [automatic commit] 2025-06-20 05:51:14 UTC 36.255.115.7

--- part2/app.js
+++ part2/app.js
@@ -170,7 +170,7 @@ app.post('/api/login', async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res.status(400).json
+        return res.status(400).json({ error: 'Username and password are requited.'})
     }
 })
 
