200268e41e9e1721960f1315c7c3c420885b9bd3
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-20 02:19:10 +0000

    [automatic commit] 2025-06-20 02:19:10 UTC 36.255.115.7

--- part1/app.js
+++ part1/app.js
@@ -10,6 +10,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+let db;
+
+
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
