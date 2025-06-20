4e61326bed39891ddd2909aa03af6e68d09fde30
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-20 02:28:20 +0000

    [automatic commit] 2025-06-20 02:28:20 UTC 36.255.115.7

--- part1/app.js
+++ part1/app.js
@@ -99,7 +99,7 @@ let db;
       ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
       ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted')
     `);
-    })
+    
 
 
 
