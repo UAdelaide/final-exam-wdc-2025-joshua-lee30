144e69be3b6d178befd814dfab5f5cfa567e9275
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-20 02:40:55 +0000

    [automatic commit] 2025-06-20 02:40:55 UTC 36.255.115.7

--- part1/app.js
+++ part1/app.js
@@ -115,10 +115,14 @@ let db;
 
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
-    res.json(books);
+    const [rows] = await db.execute(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+    res.json(rows);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 
