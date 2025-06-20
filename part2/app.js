64ae61dc231a657219d597e079f889aad2a84e69
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-20 23:47:35 +0000

    [automatic commit] 2025-06-20 23:47:35 UTC 36.255.115.7

--- part2/app.js
+++ part2/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
+var session = require('express-session');
 
 var app = express();
 
