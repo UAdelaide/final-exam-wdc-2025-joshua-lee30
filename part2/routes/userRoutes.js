d2483bca1bf865f107581107a22495f67f84953b
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-21 02:58:28 +0000

    [automatic commit] 2025-06-21 02:58:28 UTC 36.255.115.7

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -75,7 +75,7 @@ router.get('/dogs/mine', async (req, res) => {
   try {
     const [dogs] = await db.query(
       'SELECT dog_id, name FROM Dogs WHERE owner_id = (SELECT user_id FROM Users WHERE username = ?)',
-      [req.session.user.username]
+      [req.session.user.user_id]
     );
     res.json(dogs);
   } catch (error) {
