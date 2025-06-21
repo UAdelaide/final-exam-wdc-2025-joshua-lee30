b941193980d1f32f249551a3afa862151a97cc7f
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-21 01:37:59 +0000

    [automatic commit] 2025-06-21 01:37:59 UTC 36.255.115.7

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(session({
-  //  secret: 'dogwalk-secret',
+secret: 'dogwalk-secret',
     //resave: false,
    // saveUninitialized: false,
   //  cookie: { secure: false }
