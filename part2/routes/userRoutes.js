f347d9be7c0dd81afc8c92824a0055cd174bbbc2
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-21 01:10:08 +0000

    [automatic commit] 2025-06-21 01:10:08 UTC 36.255.115.7

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,7 +35,6 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST login
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
@@ -55,14 +54,18 @@ router.post('/login', async (req, res) => {
     res.cookie('expiresAt', new Date().toString());
 
     res.json({ message: 'Logged in', username });
-
+  } catch (error) {
+    res.status(500).json({ error: 'Login failed' });
+  }
 });
 
-// POST logout
 router.post('/logout', (req, res) => {
   res.clearCookie('name');
   res.clearCookie('expiresAt');
   req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
     res.json({ message: 'Logged out' });
   });
 });
