cabcab604ec352915ee9394fe608ff01fc18b62b
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-21 01:18:58 +0000

    [automatic commit] 2025-06-21 01:18:58 UTC 36.255.115.7

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -46,12 +46,12 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    const user = rows[0];
+    req.session.user = username;
 
     res.cookie('name', username);
     res.cookie('expiresAt', new Date().toString());
 
-    res.json({ message: 'Login successful', user: req.session.user });
+    res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
