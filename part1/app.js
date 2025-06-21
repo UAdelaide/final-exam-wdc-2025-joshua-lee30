ea1c98b22892db25aea872f36946333d658cbab1
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-21 03:54:34 +0000

    [automatic commit] 2025-06-21 03:54:34 UTC 36.255.115.7

--- part1/app.js
+++ part1/app.js
@@ -96,10 +96,7 @@ let db;
             await db.execute(`
     INSERT INTO Dogs (owner_id, name, size) VALUES
     ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-    ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-    ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Rocky', 'large'),
-    ((SELECT user_id FROM Users WHERE username = 'joshowner'), 'Brandy', 'medium'),
-    ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Daisy', 'small')
+    ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small')
   `);
         }
 
@@ -108,10 +105,7 @@ let db;
             await db.execute(`
     INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
     ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-    ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-    ((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 10:00:00', 60, 'City Gardens', 'open'),
-    ((SELECT dog_id FROM Dogs WHERE name = 'Brandy'), '2025-06-12 07:30:00', 30, 'Brighton Beach', 'open'),
-    ((SELECT dog_id FROM Dogs WHERE name = 'Daisy'), '2025-06-12 08:45:00', 45, 'Botanic Walk', 'completed')
+    ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted')
   `);
         }
     } catch (err) {
