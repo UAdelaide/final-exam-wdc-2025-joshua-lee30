3c5b8c095893bff5fa9174bc992a7ef8d5aa6431
Joshua Lee <joshua.t.lee@student.adelaide.edu.au>
2025-06-21 03:47:06 +0000

    [automatic commit] 2025-06-21 03:47:06 UTC 36.255.115.7

--- part1/app.js
+++ part1/app.js
@@ -99,6 +99,9 @@ let db;
     INSERT INTO Dogs (owner_id, name, size) VALUES
     ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
     ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small')
+    ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Rocky', 'large'),
+     ((SELECT user_id FROM Users WHERE username = 'joshowner'), 'Brandy', 'medium'),
+     ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Daisy', 'small')
   `);
         }
 
